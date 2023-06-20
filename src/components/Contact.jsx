35c49b30421d437d527029b379f1ee5dad85e483
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-06-19 22:21:06 -0600

    Fixed contact page and navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,50 +17,52 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn('left', 'tween', 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl xl:me-[20px] w-full xl:w-auto'
+        className='bg-black-100 p-8 rounded-2xl w-full'
       >
-        <p className={styles.sectionSubText}>Let's work together...</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
-
-        <div
-          className='flex flex-col gap-8 text-[34px] items-center text-[#c58000]'
-        >
-          <Link className='contact-link' id='profile-link' to='https://github.com/JoseMiguelVazquez' target='_blank' rel='noreferrer'>
-            <div className='flex items-center'>
-              <BsGithub className='me-2' />
-              <p>GitHub</p>
+        <div>
+          <p className={styles.sectionSubText}>Let's work together...</p>
+          <h3 className={styles.sectionHeadText}>Contact.</h3>
+        </div>
+        <div className='flex flex-col xl:flex-row items-center justify-center'>
+          <div
+            className='flex flex-col gap-8 text-[30px] my-6 md:my-4 xl:my-0 md:text-[38px] items-center xl:items-start text-[#c58000]'
+          >
+            <Link className='hover:translate-y-2 ease-out duration-300' id='profile-link' to='https://github.com/JoseMiguelVazquez' target='_blank' rel='noreferrer'>
+              <div className='flex items-center'>
+                <BsGithub className='me-2' />
+                <p>GitHub</p>
+              </div>
+            </Link>
+            <Link className='hover:translate-y-2 ease-out duration-300' to='https://www.linkedin.com/in/jos%C3%A9-miguel-v%C3%A1zquez-espinosa-35a97691/' target='_blank' rel='noreferrer'>
+              <div className='flex items-center'>
+                <BsLinkedin className='me-2' />
+                <p>LinkedIn</p>
+              </div>
+            </Link>
+            <Link className='hover:translate-y-2 ease-out duration-300' to='mailto:josemiguelvazqueze@gmail.com' target='_blank' rel='noreferrer'>
+              <div className='flex items-center'>
+                <SiMaildotru className='me-2' />
+                <p>Send an Email</p>
+              </div>
+            </Link>
+            <Link className='hover:translate-y-2 ease-out duration-300' to='tel:522223778989' target='_blank' rel='noreferrer'>
+              <div className='flex items-center'>
+                <BsPhone className='me-2' />
+                <p>Call me</p>
+              </div>
+            </Link>
+          </div>
+          <div
+            className='flex md:h-[550px] h-[350px] md:w-[650px] xl:w-[550px] items-start justify-center mt-4'
+          >
+            <div className='flex flex-col items-center h-5/6'>
+              <EarthCanvas />
+              <h4 className='italic text-[#dfd9ff] font-light lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]'>How do you take your coffee?</h4>
             </div>
-          </Link>
-          <Link className='contact-link' to='https://www.linkedin.com/in/jos%C3%A9-miguel-v%C3%A1zquez-espinosa-35a97691/' target='_blank' rel='noreferrer'>
-            <div className='flex items-center'>
-              <BsLinkedin className='me-2' />
-              <p>LinkedIn</p>
-            </div>
-          </Link>
-          <Link className='contact-link' to='mailto:josemiguelvazqueze@gmail.com' target='_blank' rel='noreferrer'>
-            <div className='flex items-center'>
-              <SiMaildotru className='me-2' />
-              <p>Send an Email</p>
-            </div>
-          </Link>
-          <Link className='contact-link' to='tel:522223778989' target='_blank' rel='noreferrer'>
-            <div className='flex items-center'>
-              <BsPhone className='me-2' />
-              <p>Call me</p>
-            </div>
-          </Link>
+          </div>
         </div>
       </motion.div>
 
-      <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
-        className='xl:flex-1 flex md:h-[550px] h-[350px] md:w-[650px] xl:w-[550px] xl:ms-[20px] items-center justify-center'
-      >
-        <div className='flex flex-col items-center h-5/6'>
-          <EarthCanvas />
-          <h4 className='italic text-[#dfd9ff] font-light lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]'>How do you take your coffee?</h4>
-        </div>
-      </motion.div>
     </div>
   )
 }
